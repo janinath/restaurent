6a7c04e348e3fbc88840337fab1721eb9a0d21c2
janinath <janinath9061@gmail.com>
2022-12-08 14:57:43 +0530

    commit2

--- restaurent/restaurent/settings.py
+++ restaurent/restaurent/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v-ky!ga2=%r_#1*ax-uziytpcj5+)z9bwm=mzu7=xskalouxbm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'restaurent.urls'
